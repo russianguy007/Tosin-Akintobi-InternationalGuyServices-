49c81a7dc7834ad34448d5cad1a78678207e8193
russianguy007 <92725565+russianguy007@users.noreply.github.com>
2022-12-13 10:56:00 +0700

    Add files via upload

---
+++ DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=UserMicroservice;TrustServerCertificate=True;Data Source=DESKTOP-PIMV8MQ\\SQLEXPRESS");
+            //Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=UserMicroservice;Data Source=DESKTOP-PIMV8MQ\SQLEXPRESS
+        }
+    }
+}
